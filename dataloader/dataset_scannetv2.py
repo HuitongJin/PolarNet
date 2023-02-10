ad9a1d1c4d5748801a40290b6f334db8a04fcb9f
HuitongJin <2019151067@email.szu.edu.cn>
2023-02-10 17:37:34 +0800

    add scannet2

--- dataloader/dataset_scannetv2.py
+++ dataloader/dataset_scannetv2.py
@@ -12,8 +12,13 @@
 import os
 import numpy as np
 import torch
-from torch.utils.data import Dataset
+from torch.utils import data
+import numba as nb
 
+
+# ===============
+# utils functions
+# ===============
 
 def pc_normalize(pc):
     centroid = np.mean(pc, axis=0)
@@ -21,6 +26,30 @@ def pc_normalize(pc):
     m = np.max(np.sqrt(np.sum(pc ** 2, axis=1)))
     pc = pc / m
     return pc
+
+
+# transformation between Cartesian coordinates and polar coordinates
+def cart2polar(input_xyz):
+    rho = np.sqrt(input_xyz[:, 0] ** 2 + input_xyz[:, 1] ** 2)
+    phi = np.arctan2(input_xyz[:, 1], input_xyz[:, 0])
+    return np.stack((rho, phi, input_xyz[:, 2]), axis=1)
+
+
+@nb.jit('u1[:,:,:](u1[:,:,:],i8[:,:])', nopython=True, cache=True, parallel=False)
+def nb_process_label(processed_label, sorted_label_voxel_pair):
+    label_size = 256
+    counter = np.zeros((label_size,), dtype=np.uint16)
+    counter[sorted_label_voxel_pair[0, 3]] = 1
+    cur_sear_ind = sorted_label_voxel_pair[0, :3]
+    for i in range(1, sorted_label_voxel_pair.shape[0]):
+        cur_ind = sorted_label_voxel_pair[i, :3]
+        if not np.all(np.equal(cur_ind, cur_sear_ind)):
+            processed_label[cur_sear_ind[0], cur_sear_ind[1], cur_sear_ind[2]] = np.argmax(counter)
+            counter = np.zeros((label_size,), dtype=np.uint16)
+            cur_sear_ind = cur_ind
+        counter[sorted_label_voxel_pair[i, 3]] += 1
+    processed_label[cur_sear_ind[0], cur_sear_ind[1], cur_sear_ind[2]] = np.argmax(counter)
+    return processed_label
 
 
 # ================
@@ -225,9 +254,10 @@ def collate_fn_points(data):
 # ============================
 # Define scannet dataset class
 # ============================
-class ScannetDataset(Dataset):
+class ScannetDataset(data.Dataset):
     def __init__(self, path, split='train', return_ref=False):
         super().__init__()
+        self.return_ref = return_ref
         self.label_to_names = ScannetV2_label_name
         self.label_values = np.sort(np.sort([k for k, v in self.label_to_names.items()]))
         self.label_to_idx = {l: i for i, l in enumerate(self.label_values)}
@@ -261,8 +291,119 @@ class ScannetDataset(Dataset):
         return len(self.files)
 
 
-class spherical_dataset:
-    pass
+class spherical_dataset(data.Dataset):
+    def __init__(self, in_dataset, grid_size, rotate_aug=False, flip_aug=False, ignore_label=255, return_test=False,
+                 fixed_volume_space=False, max_volume_space=[50, np.pi, 1.5], min_volume_space=[3, -np.pi, -3]):
+        """Initialization"""
+        self.point_cloud_dataset = in_dataset
+        self.grid_size = np.asarray(grid_size)
+        self.rotate_aug = rotate_aug
+        self.flip_aug = flip_aug
+        self.ignore_label = ignore_label
+        self.return_test = return_test
+        self.fixed_volume_space = fixed_volume_space
+        self.max_volume_space = max_volume_space
+        self.min_volume_space = min_volume_space
+
+    def __len__(self):
+        """Denotes the total number of samples"""
+        return len(self.point_cloud_dataset)
+
+    def __getitem__(self, index):
+        """Generates one sample of data"""
+        data = self.point_cloud_dataset[index]
+        if len(data) == 2:
+            xyz, labels = data
+        elif len(data) == 3:
+            xyz, labels, sig = data
+            if len(sig.shape) == 2:
+                sig = np.squeeze(sig)
+        else:
+            raise Exception('Return invalid data tuple')
+
+        # random data augmentation by rotation
+        if self.rotate_aug:
+            rotate_rad = np.deg2rad(np.random.random() * 360)
+            c, s = np.cos(rotate_rad), np.sin(rotate_rad)
+            j = np.matrix([[c, s], [-s, c]])
+            xyz[:, :2] = np.dot(xyz[:, :2], j)
+
+        # random data augmentation by flip x , y or x+y
+        if self.flip_aug:
+            flip_type = np.random.choice(4, 1)
+            if flip_type == 1:
+                xyz[:, 0] = -xyz[:, 0]
+            elif flip_type == 2:
+                xyz[:, 1] = -xyz[:, 1]
+            elif flip_type == 3:
+                xyz[:, :2] = -xyz[:, :2]
+
+        # convert coordinate into polar coordinates
+        xyz_pol = cart2polar(xyz)
+
+        max_bound_r = np.percentile(xyz_pol[:, 0], 100, axis=0)
+        min_bound_r = np.percentile(xyz_pol[:, 0], 0, axis=0)
+        max_bound = np.max(xyz_pol[:, 1:], axis=0)
+        min_bound = np.min(xyz_pol[:, 1:], axis=0)
+        max_bound = np.concatenate(([max_bound_r], max_bound))
+        min_bound = np.concatenate(([min_bound_r], min_bound))
+        if self.fixed_volume_space:
+            max_bound = np.asarray(self.max_volume_space)
+            min_bound = np.asarray(self.min_volume_space)
+
+        # get grid index
+        crop_range = max_bound - min_bound
+        cur_grid_size = self.grid_size
+        intervals = crop_range / (cur_grid_size - 1)
+
+        if (intervals == 0).any():
+            print("Zero interval!")
+        grid_ind = (np.floor((np.clip(xyz_pol, min_bound, max_bound) - min_bound) / intervals)).astype(np.int)
+
+        # process voxel position
+        voxel_position = np.zeros(self.grid_size, dtype=np.float32)
+        dim_array = np.ones(len(self.grid_size) + 1, int)
+        dim_array[0] = -1
+        voxel_position = np.indices(self.grid_size) * intervals.reshape(dim_array) + min_bound.reshape(dim_array)
+        # voxel_position = polar2cat(voxel_position)
+
+        # process labels
+        processed_label = np.ones(self.grid_size, dtype=np.uint8) * self.ignore_label
+        label_voxel_pair = np.concatenate([grid_ind, labels], axis=1)
+        label_voxel_pair = label_voxel_pair[np.lexsort((grid_ind[:, 0], grid_ind[:, 1], grid_ind[:, 2])), :]
+        processed_label = nb_process_label(np.copy(processed_label), label_voxel_pair)
+        # data_tuple = (voxel_position,processed_label)
+
+        # prepare visiblity feature
+        # find max distance index in each angle,height pair
+        valid_label = np.zeros_like(processed_label, dtype=bool)
+        valid_label[grid_ind[:, 0], grid_ind[:, 1], grid_ind[:, 2]] = True
+        valid_label = valid_label[::-1]
+        max_distance_index = np.argmax(valid_label, axis=0)
+        max_distance = max_bound[0] - intervals[0] * (max_distance_index)
+        distance_feature = np.expand_dims(max_distance, axis=2) - np.transpose(voxel_position[0], (1, 2, 0))
+        distance_feature = np.transpose(distance_feature, (1, 2, 0))
+        # convert to boolean feature
+        distance_feature = (distance_feature > 0) * -1.
+        distance_feature[grid_ind[:, 2], grid_ind[:, 0], grid_ind[:, 1]] = 1.
+
+        data_tuple = (distance_feature, processed_label)
+
+        # center data on each voxel for PTnet
+        voxel_centers = (grid_ind.astype(np.float32) + 0.5) * intervals + min_bound
+        return_xyz = xyz_pol - voxel_centers
+        return_xyz = np.concatenate((return_xyz, xyz_pol, xyz[:, :2]), axis=1)
+
+        if len(data) == 2:
+            return_fea = return_xyz
+        elif len(data) == 3:
+            return_fea = np.concatenate((return_xyz, sig[..., np.newaxis]), axis=1)
+
+        if self.return_test:
+            data_tuple += (grid_ind, labels, return_fea, index)
+        else:
+            data_tuple += (grid_ind, labels, return_fea)
+        return data_tuple
 
 
 if __name__ == '__main__':
